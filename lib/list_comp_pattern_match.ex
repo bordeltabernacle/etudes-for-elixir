bc5d8241050ac94a39fe103242fa79a55a4ba915
Rob Phoenix <robertphoenix@gmail.com>
2016-01-07 10:21:44 +0000

    completed Etude 8-2

--- lib/list_comp_pattern_match.ex
+++ lib/list_comp_pattern_match.ex
@@ -1,10 +1,28 @@
 defmodule ListCompPatternMatch do
+  @moduledoc """
+  Module to try pattern matching in list comprehensions
+  """
 
-  def male_and_over_40(list) do
-    
+  @doc """
+  Select all males older than 40 from a list of tuples giving
+  name, gender, and age.
+  """
+  @spec males_over_40(list(tuple())) :: list
+  def males_over_40(list) do
+    for {name, "M", age} <- list, age > 40 do
+      name
+    end
   end
 
+  @doc"""
+  Select all people who are male or older than 40 from a list of
+  tuples giving name, gender, and age.
+  """
+  @spec male_or_over_40(list(tuple())) :: list
   def male_or_over_40(list) do
-    
+    for {name, gender, age} <- list,
+    gender == "M" or age > 40 do
+      name
+    end
   end
 end
--- test/list_comp_pattern_match_test.exs
+++ test/list_comp_pattern_match_test.exs
@@ -1,16 +1,17 @@
 defmodule ListCompPatternMatchTest do
 	use ExUnit.Case
+  alias ListCompPatternMatch, as: L
 
   @people [{"Federico", "M", 22}, {"Kim", "F", 45}, {"Hansa", "F", 30},
            {"Tran", "M", 47}, {"Cathy", "F", 32}, {"Elias", "M", 50}]
 
-  test "return list of names of those male and over 40" do
+  test "return list of names of males over 40" do
     expected = ["Tran", "Elias"]
-    assert ListCompPatternMatch.male_and_over_40(@people) == expected
+    assert L.males_over_40(@people) == expected
   end
 
   test "return list of names of those male or over 40" do
     expected = ["Federico", "Kim", "Tran", "Elias"]
-    assert ListCompPatternMatch.male_or_over_40(@people) == expected
+    assert L.male_or_over_40(@people) == expected
   end
 end
